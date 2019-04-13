bedb5a73b490e2fc46e8b96c8acbcc0602b7f08f
susc <995318732@qq.com>
2019-04-13 22:30:36 +0800

    add some admin function

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,15 +2,17 @@
     <div>
         <admin-header></admin-header>
         <admin-general></admin-general>
+        <admin-link></admin-link>
     </div>
 </template>
 
 <script>
 const AdminHeader = () => import('~/components/admin-header.vue');
 const AdminGeneral = () => import('~/components/admin-general.vue');
+const AdminLink = () => import('~/components/admin-link.vue');
 
 export default {
-  components: {AdminHeader, AdminGeneral}
+  components: { AdminHeader, AdminGeneral, AdminLink }
 }
 </script>
 
